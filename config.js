ff5160fa58fdb5bd05c63fc485ad5d9c2838d7b3
sbecot <simon.becot@orange.com>
2017-01-24 16:40:55 +0100

    change in config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {}
 
 config.node = {}
-config.node.host = "localhost"
+config.node.port = "localhost" 
 config.node.port = process.env.NODE_PORT || 8080
 config.redis = {}
-config.redis.host = process.env.REDIS_PORT_6379_TCP_ADDR || "localhost"
+config.redis.host = process.env.REDIS_PORT_6379_TCP_ADDR || "redis"
 config.redis.port = process.env.REDIS_PORT_6379_TCP_PORT || 6379
 
 
